a392810b36d6db02141da5ddd57e9961b83b8c99
PARK <bht9011@gmail.com>
2022-02-09 21:03:07 +0900

    commit

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,55 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    /* 회원 서비스가 메모리 회원 리포지토리를 직접 생성 */
+//    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /* But 변경된 코드에서는 회원 리포지토리의 코드가 회원 서비스 코드를 DI가능하게 변경했다. */
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     * @param member
+     * @return
+     */
+    public Long join (Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -4,7 +4,10 @@ import hello.hellospring.domain.Member;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 class MemoryMemberRepositoryTest {
 
@@ -28,5 +31,42 @@ class MemoryMemberRepositoryTest {
         // then
         Member result = repository.findById(member.getId()).get();
         assertThat(result).isEqualTo(member);
+    }
+
+    @Test
+    public void findByName() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        Member member2 = new Member();
+        member2.setName("spring2");
+
+        repository.save(member1);
+        repository.save(member2);
+
+        // when
+        Member result = repository.findByName("spring1").get();
+
+        // then
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        // when
+        List<Member> result = repository.findAll();
+
+        // then
+        assertThat(result.size()).isEqualTo(2);
+
     }
 }
