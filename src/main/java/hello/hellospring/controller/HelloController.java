412e71a5e669bf8411bee0540f7dd5b21759afc8
PARK <bht9011@gmail.com>
2022-02-09 09:02:34 +0900

    commit!

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -46,4 +46,6 @@ public class HelloController {
             this.name = name;
         }
     }
+
+
 }
---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,32 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+
+        // given
+        Member member = new Member();
+        member.setName("spring");
+
+        // when
+        repository.save(member);
+
+        // then
+        Member result = repository.findById(member.getId()).get();
+        assertThat(result).isEqualTo(member);
+    }
+}
